45927de5315f8214498be6d512a491abe9dda5ff
noobmastter <132351920+noobmastter@users.noreply.github.com>
2025-03-23 19:54:56 +0530

    Create web_server.py

---
+++ plugins/web_server.py
@@ -0,0 +1,9 @@
+from aiohttp import web
+
+async def index(request):
+    return web.Response(text="Webhook is running!", status=200)
+
+async def web_server():
+    app = web.Application()
+    app.router.add_get("/", index)
+    return app
