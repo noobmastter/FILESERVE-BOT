8c4aabb32683e1e535b947180532ad34060bf6ce
noobmastter <132351920+noobmastter@users.noreply.github.com>
2025-03-24 12:22:12 +0530

    Update web_server.py

--- plugins/web_server.py
+++ plugins/web_server.py
@@ -1,8 +1,6 @@
 import os
 from aiohttp import web
 
-PORT = int(os.getenv("PORT", 8000))  # Get port from environment (default: 8000)
-
 async def index(request):
     return web.Response(text="Webhook is running!", status=200)
 
